538df8f44b5c03d0db927ec4374da1e9277f63e0
Dmitry Sviridenko <letehaha@gmail.com>
2020-08-04 19:43:16 +0300

    Fix: nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,10 @@
 // https://github.com/vuex-orm/vuex-orm/issues/255#issuecomment-457619889
-// This prevents displaying useless "Cannot stringify arbitrary non-POJOs" warning
-process.env.NUXT_ENV_DEVALUE_LOG_LIMIT = -1
+// This prevents displaying useless "Cannot stringify arbitrary non-POJOs"
+// warning
+process.env.NUXT_ENV_DEVALUE_LOG_LIMIT = -1;
 
 module.exports = {
-  mode: 'ssr',
+  mode: 'universal',
   modules: [
     '@nuxtjs/style-resources',
     '@nuxtjs/svg',
@@ -11,7 +12,7 @@ module.exports = {
     [
       'nuxt-i18n',
       {
-        locales: config.SUPPORTED_LANGUAGES,
+        locales: 'en',
         defaultLocale: 'en',
         langDir: 'locales/',
         lazy: true,
@@ -34,15 +35,33 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js base project' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js base project',
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon-16x16.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon-32x32.png' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: 'favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: 'favicon-32x32.png',
+      },
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: 'apple-touch-icon.png',
+      },
       { rel: 'mask-icon', href: 'safari-pinned-tab.svg', color: 'white' },
-    ]
+    ],
   },
   /*
   ** Customize the progress bar color
@@ -59,7 +78,7 @@ module.exports = {
   ** Global styles
   */
   css: [
-    '~/assets/styles/index.scss'
+    '~/assets/styles/index.scss',
   ],
   /*
   ** Global scss utils (mixins, vars, virtual-classes)
@@ -67,7 +86,7 @@ module.exports = {
   styleResources: {
     scss: [
       '~/assets/styles/resources/index.scss',
-    ]
+    ],
   },
   /*
   ** Browserconfig settings
@@ -75,13 +94,13 @@ module.exports = {
   browserconfig: {
     tile: {
       TileColor: '#da532c',
-      square70x70logo: {'@':{src:'static/mstile-70x70.png'}},
-      square144x144logo: {'@':{src:'static/mstile-144x144.png'}},
-      square150x150logo: {'@':{src:'static/mstile-150x150.png'}},
-      wide310x150logo: {'@':{src:'static/mstile-310x150.png'}},
-      square310x310logo: {'@':{src:'static/mstile-310x310.png'}},
+      square70x70logo: { '@': { src: 'static/mstile-70x70.png' } },
+      square144x144logo: { '@': { src: 'static/mstile-144x144.png' } },
+      square150x150logo: { '@': { src: 'static/mstile-150x150.png' } },
+      wide310x150logo: { '@': { src: 'static/mstile-310x150.png' } },
+      square310x310logo: { '@': { src: 'static/mstile-310x310.png' } },
       applicationName: 'Example',
-    }
+    },
   },
   /*
   ** PWA settings
@@ -106,7 +125,7 @@ module.exports = {
           src: 'android-chrome-512x512.png',
           sizes: '512x512',
           type: 'image/png',
-        }
+        },
       ],
     },
   },
@@ -117,16 +136,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+          exclude: /(node_modules)/,
+        });
       }
-    }
-  }
-}
-
+    },
+  },
+};
