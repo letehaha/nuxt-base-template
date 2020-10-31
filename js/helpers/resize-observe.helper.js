bcf08237c4aae19d8ee05381fc27e97332ee3b7c
Dmitry Sviridenko <letehaha@gmail.com>
2020-10-31 19:25:17 +0200

    Feat: add type checks to observeDOMNode

--- js/helpers/resize-observe.helper.js
+++ js/helpers/resize-observe.helper.js
@@ -1,4 +1,10 @@
 export const observeDOMNode = (DOMNode, callback) => {
+  if (!(DOMNode instanceof HTMLElement)) {
+    throw new Error('observeDOMNode helper: "DOMNode" should be an HTMLElement');
+  }
+  if (typeof callback !== 'function') {
+    throw new Error('observeDOMNode helper: "callback" should be an Function');
+  }
   const resizeObserver = new ResizeObserver(() => {
     callback();
   });
