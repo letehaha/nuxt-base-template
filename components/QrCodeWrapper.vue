9bbd3e6e96e45bd255f0c5d4e82851db3998d9fc
Dmitry Sviridenko <letehaha@gmail.com>
2020-11-27 14:06:54 +0200

    Feat: add QrCodeWrapper component

---
+++ components/QrCodeWrapper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="qr-code__wrapper"
+    :style="{
+      padding: `${padding}${paddingDimension}`,
+      background,
+    }"
+  >
+    <qr-code
+      :value="value"
+      :size="size - (padding * 2)"
+      :background="background"
+      :foreground="foreground"
+      :class-name="className"
+      :level="level"
+    />
+  </div>
+</template>
+
+<script>
+import QrCode from 'qrcode.vue';
+
+export default {
+  components: {
+    QrCode,
+  },
+  props: {
+    value: { type: String, default: '' },
+    size: { type: Number, default: 200 },
+    background: { type: String, default: '#fff' },
+    foreground: { type: String, default: '#262626' },
+    className: { type: String, default: '' },
+    level: { type: String, default: 'L' },
+    padding: { type: Number, default: 5 },
+    paddingDimension: { type: String, default: 'px' },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.qr-code__wrapper {
+  border-radius: 6px;
+}
+</style>
